8fd9d46c18a496fa648256d52d746b38ec15a982
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2018-06-13 16:52:50 +0900

    it can run

--- game_of_life.py
+++ game_of_life.py
@@ -1,8 +1,10 @@
 #
 # Conway's Game of Life implementation for micro:bit
+# https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 #
 
 import random
+from microbit import *
 
 
 class GameOfLife:
@@ -29,9 +31,46 @@ class GameOfLife:
 
         print("end of init")
 
-    def execute(self):
-        self.current_iteration += 1
+    def count_alive_cell(self, x, y):
+        alive_cell_num = 0
+
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if (i, j) != (x, y) and self.current_state[i][j] == GameOfLife.cell_state['ALIVE']:
+                    alive_cell_num += 1
+
+        return alive_cell_num
+
+    def update_cells(self):
+        for i in range(1, GameOfLife.CELL_NUM - 1):
+            for j in range(1, GameOfLife.CELL_NUM - 1):
+
+                current_cell_state = self.current_state[i][j]
+                alive_cell_num = self.count_alive_cell(i, j)
+
+                if (current_cell_state == GameOfLife.cell_state['DEAD'] or
+                    current_cell_state == GameOfLife.cell_state['DYING']) and alive_cell_num == 3:
+                    self.current_state[i][j] = GameOfLife.cell_state['ALIVE']
+                elif current_cell_state == GameOfLife.cell_state['ALIVE'] and alive_cell_num <= 1:
+                    self.current_state[i][j] = GameOfLife.cell_state['DYING']
+                elif current_cell_state == GameOfLife.cell_state['ALIVE'] and alive_cell_num >= 4:
+                    self.current_state[i][j] = GameOfLife.cell_state['DYING']
+                elif self.current_state[i][j] == GameOfLife.cell_state['DYING']:
+                    self.current_state[i][j] = GameOfLife.cell_state['DEAD']
+
+    def show_led(self):
+        for i in range(1, GameOfLife.CELL_NUM - 1):
+            for j in range(1, GameOfLife.CELL_NUM - 1):
+                current_cell_state = self.current_state[i][j]
+
+                if current_cell_state == GameOfLife.cell_state['ALIVE']:
+                    display.set_pixel(i - 1, j - 1, 9)
+                elif current_cell_state == GameOfLife.cell_state['DYING']:
+                    display.set_pixel(i - 1, j - 1, 5)
+                else:
+                    display.set_pixel(i - 1, j - 1, 0)
 
 
 # MicroPython dose not have __main__
 goe = GameOfLife()
+
