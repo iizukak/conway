a8c20b2bb5e2864f26ee676d4351f724a75ad92b
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2018-06-13 09:43:28 +0900

    miropython dose not have enum

--- game_of_life.py
+++ game_of_life.py
@@ -2,7 +2,6 @@
 # Conway's Game of Life implementation for micro:bit
 #
 
-from enum import Enum, auto
 import random
 
 
@@ -10,26 +9,29 @@ class GameOfLife:
     CELL_NUM = 7
     MAX_ITERATION = 100
 
-    class CellState(Enum):
-        ALIVE = auto()
-        DEAD = auto()
-        DYING = auto()
+    # MicroPython dose not have enum module
+    cell_state = {
+        'ALIVE': 0,
+        'DEAD': 1,
+        'DYING': 2
+    }
 
     def __init__(self):
         self.current_iteration = 0
 
         # current_state is 2 dimensional array of CellState
         self.current_state = \
-            [[GameOfLife.CellState.DEAD for _ in range(GameOfLife.CELL_NUM)] for _ in range(GameOfLife.CELL_NUM)]
+            [[GameOfLife.cell_state['DEAD'] for _ in range(GameOfLife.CELL_NUM)] for _ in range(GameOfLife.CELL_NUM)]
 
         for i in range(1, GameOfLife.CELL_NUM - 1):
             for j in range(1, GameOfLife.CELL_NUM - 1):
-                self.current_state[i][j] = random.choice(list(GameOfLife.CellState))
+                self.current_state[i][j] = random.randint(0, 2)
+
+        print("end of init")
 
     def execute(self):
         self.current_iteration += 1
 
 
-if __name__ == '__main__':
-    g = GameOfLife().current_state
-    print(g)
+# MicroPython dose not have __main__
+goe = GameOfLife()
